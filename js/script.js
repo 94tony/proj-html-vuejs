1bbd3023f0d12ac74a187619595cf783957d3d8e
94tony <dagatatony94@gmail.com>
2021-07-22 10:09:45 +0200

    struttura 2.0

--- js/script.js
+++ js/script.js
@@ -1,16 +1,77 @@
 const app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello world',
-    },
+       
+                home:
+                    {
+                      primo: " 01. Home Minimal",
+                      secondo:" 02. Home Season" ,
+                      terzo:" 03. Home Classic",
+                      quarto:"04. Home Collection",
+                      quinto:"005. Home Categories",
+                      sesto:"06. Home Best Selling",
+                      settimo:"07. Home Lookbook",
+                      ottavo:"  08. Home Strong",
+                      nono:"09. Home Stylish",
+                      decimo:" 10. Home Sticky Menu"
+                    },
+
+                   
+               
+
+                shop:
+                    {
+                            
+                        a:"SHOP STYLE",
+                        b:"Shop – Fullwidth",
+                        c:"SHOP STYLE",
+                        d:"Shop – Fullwidth",
+                        e:"Shop – Sidebar",
+                        f:"Shop – Listview",
+                        g:"PRODUCT STYLE",
+                        h:"Product Style 1",
+                        i: "Product Style 2",
+                        l:"Product Style 3",
+                        m:"PRODUCT LAYOUTS",
+                        n:"Vertical Thumbnail",
+                        o:"Horizontal Thumbnail",
+                        p:"Gallery Thumbnail",
+                        q:"Flat Thumbnail",
+                        r:"OTHER PAGES",
+                        s:"Cart",
+                        u:"Wishlist",
+                        w:"checkout",
+                        z:"My account"
+                    
+
+                    },
+                page:
+                    {
+                        a:"About us",
+                        b:"Faq",
+                        c:"404",
+                        d:"icons"
+
+                    } ,
+               
+               
+                blog:
+                    {
+                        a:"Blog Standard",
+                        b:"Blog list",
+                        c:"Blog Overlay",
+                        d:"Blog Masonry",
+                        e:"Single Blog",
+                        
+
+                    }   
+                   
+                   
+                    
+             },
+    
     methods: {
-      capitalize () {
-        return this.message.toUpperCase()
-      }
+     
     },
-    computed: {
-      reversedMessage () {
-        return this.message.split('').reverse().join('')
-      }
-    }
+    
   })
